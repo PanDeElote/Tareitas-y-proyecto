79b1da12587d2a886a7c1ca9c7d5ab2b9d436091
PanDeElote <verdisa17@gmail.com>
2025-08-13 12:35:32 -0600

    Add files via upload

---
+++ ecommerce-api/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './src/routes/index.js';
+import dbConnection from './src/config/database.js';
+import logger from './src/middlewares/logger.js';
+import errorHandler from './src/middlewares/errorHandler.js';
+import setupGlobalErrorHandlers from './src/middlewares/globalErrorHandler.js';
+
+dotenv.config();
+
+setupGlobalErrorHandlers();
+
+const app = express();
+dbConnection();
+
+app.use(express.json());
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('WELCOME!');
+});
+
+app.use('/api', routes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Ruta no encontrada',
+    method: req.method,
+    url: req.originalUrl
+  });
+});
+
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
